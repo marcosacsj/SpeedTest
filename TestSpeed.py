855303c833c309b6b195dff68283295067f7dc6c
Marcos Junior <marcosacsj@yahoo.com.br>
2021-04-05 11:24:23 -0300

    Inicio Implementação
    Estudo e início da implementação de teste te upload.

--- TestSpeed.py
+++ TestSpeed.py
@@ -9,10 +9,12 @@ def internet():
     s = speedtest.Speedtest()
     data_atual = datetime.now().strftime('%d/%m/%Y')
     hora_atual = datetime.now().strftime('%H:%M')
-    velocidade = s.download(threads=None)*(10**-6)
-    df.loc[len(df)] = [data_atual, hora_atual, round(velocidade)]
+    velocidade_down = s.download(threads=None)*(10**-6)
+    velocidade_up = s.upload(threads=None)*(10**-6)
+    teste_ = "teste"
+    df.loc[len(df)] = [data_atual, hora_atual, round(velocidade_down), round(velocidade_up), teste_ ]
     df.to_excel('dados.xlsx', sheet_name='base', index=False)
-    Timer(30,internet).start()
+    Timer(5,internet).start()
 
 
 internet()
